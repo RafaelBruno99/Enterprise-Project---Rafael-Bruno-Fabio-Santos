8d71d121b63b5baa9ea7ec294f568ea8fba558eb
fabio98984 <45598007+fabio98984@users.noreply.github.com>
2021-02-11 20:42:04 +0000

    Add files via upload

--- employer/employer-area.php
+++ employer/employer-area.php
@@ -3,18 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Tutor</title>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
-	<link href="../css/student.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <link href="../student/student.css" rel="stylesheet">
+
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 	<div class="box-area">
 		<header>
 			<div class="wrapper">
 				<div class="logo">
-					<a href="../tutor-area.php">APPRENTICESHIP</a>
+					<a href="../tutor/tutor-area.php">APPRENTICESHIP</a>
 				</div>
 				<nav>
 					<a href="employer-student.php">STUDENTS</a>
+                    <a href="../logout.php"><button type="button" class="btn btn-success" >Logout</button></a>
 				</nav>
 			</div>
 		</header>
--- employer/employer-student.php
+++ employer/employer-student.php
@@ -14,15 +14,14 @@
 
   	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
-  	<link href="../css/student.css" rel="stylesheet">
+  	<link href="../student/student.css" rel="stylesheet">
 
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 
-  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   	<link rel="stylesheet" href="../css/style.css">
 
-    <link rel="stylesheet" href="css/button.css">
 </head>
 <body>
 	<div class="box-area">
@@ -33,6 +32,7 @@
 				</div>
 				<nav>
           <a href="employer-student.php">STUDENTS</a>
+                    <a href="../logout.php"><button type="button" class="btn btn-success" >Logout</button></a>
 				</nav>
 			</div>
 		</header>
